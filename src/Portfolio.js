fc9276668744bac9485c3bbc045f401fc4b26de6
ESedrak <elizabeth_sedrak@hotmail.com>
2022-04-28 10:11:57 +1000

    Changing format of portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import Summary from "./Summary";
 import Skill from "./Skill";
+import "./Portfolio.css";
 
 export default function Portfolio() {
   return (
