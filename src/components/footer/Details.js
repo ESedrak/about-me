5f5d9bf5fac0f57e981c71d767dfe6f943bb7826
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-12 17:36:52 +1000

    style footer

--- src/components/footer/Details.js
+++ src/components/footer/Details.js
@@ -6,11 +6,10 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 function Details() {
   return (
     <div className="Details">
-      <h2>Contact me</h2>
+      <h2>Contact</h2>
       <span className="Details-info">✉ elizabeth.sedrak94@gmail.com</span>
       <span className="Details-info">
-        <FontAwesomeIcon icon={faPhone} />
-        0421621826
+        <FontAwesomeIcon icon={faPhone} /> 0421621826
       </span>
       <hr />
     </div>
