baf29d4170bef3a60f187fdf62c0f6d810b12e0d
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-12 17:48:09 +1000

    add interest file

--- src/components/main/Interests.js
+++ src/components/main/Interests.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Interests() {
-  return My interests
+  return (
+    <section className="Interests">
+      <h2>Interests</h2>
+      <hr />
+      <ul rol="presentation">
+        <li>Mixologist (Cocktails)</li>
+        <li>Guitar</li>
+        <li>Gardening</li>
+        <li>Puzzles</li>
+        <li>Surf Skating</li>
+      </ul>
+    </section>
+  );
 }
 
-export default Interests
+export default Interests;
--- src/components/main/Summary.js
+++ src/components/main/Summary.js
@@ -3,6 +3,7 @@ import Training from "./Training";
 import Skills from "./Skills";
 import Personal from "./Personal";
 import Professional from "./Professional";
+import Interests from "./Interests";
 import "./Summary.css";
 
 function Summary() {
@@ -15,6 +16,8 @@ function Summary() {
       <Training />
       <br />
       <Professional />
+      <br />
+      <Interests />
     </div>
   );
 }
