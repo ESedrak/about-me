37c28bea3a432ba68340ef381918c27bd63affef
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-12 16:58:20 +1000

    made new header component (called banner)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import "./App.css";
 import Portfolio from "./components/main/Portfolio";
 import Contact from "./components/footer/Contact";
+import Banner from "./components/header/Banner";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Elizabeth Sedrak</h1>
-        <h4>
-          <a
-            className="App-link"
-            href="https://sparkling-pasca-b70e55.netlify.app/"
-          >
-            {" "}
-            My Portfolio
-          </a>
-        </h4>
+      <header>
+        <Banner />
       </header>
       <main>
         <Portfolio />
---
+++ src/components/header/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <section className="Banner">
+      <h1>Elizabeth Sedrak</h1>
+      <a
+        className="Banner-link"
+        href="https://sparkling-pasca-b70e55.netlify.app/"
+      >
+        My Portfolio
+      </a>
+    </section>
+  );
+}
+
+export default Banner;
