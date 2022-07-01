9637ba82a638607256024446768ebfc456dec9de
ESedrak <elizabeth_sedrak@hotmail.com>
2022-07-01 10:55:23 +1000

    update highlights for professional experience

--- src/components/main/Professional.js
+++ src/components/main/Professional.js
@@ -7,8 +7,29 @@ function Professional() {
       <hr />
       <h3>Nuclear Medicine Technologist - Healthcare/Lumus Imaging</h3>
       <p>April 2019 - March 2022</p>
+      <p>
+        <strong>Highlights: </strong>
+        <ul role="presentation">
+          <li>
+            Rostered around many different clinics (gained knowledge with
+            different medical equipment/systems)
+          </li>
+          <li>Eluted the Gallium-68 Radiation Generator</li>
+        </ul>
+      </p>
       <h3>Nuclear Medicine Technologist - Peninsula Health</h3>
       <p> Feb 2018- Feb 2019</p>
+      <strong>Highlights: </strong>
+      <ul role="presentation">
+        <li>
+          Created the Nuclear Medicine Hot Lab protocol manual to help instruct
+          how to properly use different radiopharmaceuticals.
+        </li>
+        <li>
+          Helped liaised in creating the Nuclear Medicine Document Protocol,
+          Discharge Information and Consent forms.
+        </li>
+      </ul>
       <h3>Nuclear Medicine Intern - Hospital Internships</h3>
       <p>
         Monash Health: Moorabin and Clayton Hospital, LaTrobe Regional Hospital
