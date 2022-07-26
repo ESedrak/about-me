fff4132286446a46ddea681512da133feed5f2c6
ESedrak <elizabeth_sedrak@hotmail.com>
2022-07-26 17:00:29 +1000

    removed some info

--- src/components/footer/Details.js
+++ src/components/footer/Details.js
@@ -4,13 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 function Details() {
-  return (
-    <div className="Details">
-      <h2>Contact</h2>
-      <span className="Details-info">✉ elizabeth.sedrak94@gmail.com</span>
-      <hr />
-    </div>
-  );
+	return (
+		<div className="Details">
+			<h2>Contact</h2>
+			<span className="Details-info">✉ elizabeth.sedrak94@gmail.com</span>
+			<hr />
+		</div>
+	);
 }
 
 export default Details;
